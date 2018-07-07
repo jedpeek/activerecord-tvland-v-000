ff9fce0abf5ce890ac948b083b5db72b75cdd993
Jed Peek <jedpeek@yahoo.com>
2018-07-07 16:44:52 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,7 +3,7 @@ class Actor < ActiveRecord::Base
   has_many :characters, through: :shows
 
   def full_name
-    puts "#{self.first_name} #{self.last_name}"
+    return "#{self.first_name} #{self.last_name}"
   end
 
 end
